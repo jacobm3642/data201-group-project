cd4655ebaea2f7368a43ccfeae49781fbd4cc679
EthanWijaya123 <ewi79@uclive.ac.nz>
2023-09-22 13:49:37 +1200

    Added a couple more plots to the R file

--- bmi_data201.R
+++ bmi_data201.R
@@ -2,4 +2,13 @@ library(tidyverse)
 
 data <- read.csv("bmi_data.csv")
 
-ggplot(data, aes(ParentLocation, FactValueNumeric)) + geom_point()
+#boxplot of regions and bmi
+ggplot(data, aes(ParentLocation, FactValueNumeric)) + geom_boxplot()
+
+ggplot(data, aes(Period, FactValueNumeric, col=Location)) + geom_point()
+
+
+#Only look at countries in south east asia:
+south_east_asia <- data %>%
+  filter(ParentLocation == 'South-East Asia')
+ggplot(south_east_asia, aes(Period, FactValueNumeric, col=Location)) + geom_point()
